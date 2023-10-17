469613e45fc5fc729aae7ad47b5c0ec7492212c2
linch90 <zlchu@outlook.com>
2023-10-17 10:36:39 +0800

    upgrade to angular 16

--- host/Unihanlin.AbpDownloadService.HttpApi.Host/Migrations/20231017021937_Upgrade_To_Abp740.Designer.cs
+++ host/Unihanlin.AbpDownloadService.HttpApi.Host/Migrations/20231017021937_Upgrade_To_Abp740.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Unihanlin.AbpDownloadService.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Unihanlin.AbpDownloadService.Migrations
 {
     [DbContext(typeof(AbpDownloadServiceHttpApiHostMigrationsDbContext))]
-    partial class AbpDownloadServiceHttpApiHostMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017021937_Upgrade_To_Abp740")]
+    partial class Upgrade_To_Abp740
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
